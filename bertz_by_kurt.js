ea43e7d3455235bbedc2f2666447c67ca0603cd4
Christopher Smith <chrisronsmith@gmail.com>
2019-11-27 16:59:47 -0500

    Prevent button click bubbling

--- bertz_by_kurt.js
+++ bertz_by_kurt.js
@@ -1,18 +1,15 @@
 var audioContext = new AudioContext()
 var sourceNode
-
-
+var btnStart = document.getElementById('btn-start')
 
 fetch('brwsr2_fork_compressed.m4a')
   .then(response => response.arrayBuffer())
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
   .then(audioBuffer => {
-    var btnStart = document.getElementById('btn-start')
     var marker = document.getElementById('marker')
     btnStart.disabled = false
-    btnStart.addEventListener('click', () => {
+    btnStart.addEventListener('click', (event) => {
       audioContext.resume()
-
       if (sourceNode) {
         sourceNode.stop()
         sourceNode = null
@@ -29,7 +26,6 @@ fetch('brwsr2_fork_compressed.m4a')
         btnStart.classList.add('playing')
         marker.classList.add('playing')
         marker.style.transform = 'translate(calc(50vw - var(--marker-radius)), calc(50vh - var(--marker-radius)))'
-
       }
     })
   })
@@ -37,22 +33,13 @@ fetch('brwsr2_fork_compressed.m4a')
     console.log(e)
   })
 
-
-
 var moveMarker = (event) => {
   var marker = document.getElementById('marker')
   if (!sourceNode) return
-
   var x = event.x || event.touches[0].clientX
   var y = event.y || event.touches[0].clientY
-
   var percentY = 1 - y / window.innerHeight
-  var newAnimationDuration = (percentY + .5) ** 2
-
-  // var message = document.createElement('p')
-  // document.body.appendChild(message)
-  // message.innerText = event.touches[0].clientY
-
+  var newAnimationDuration = (percentY + .5) ** 2.5
   // marker.style.transform = `translate(calc(${x}px - var(--marker-radius)), calc(${y}px - var(--marker-radius)))`
   marker.style.transform = `translate(calc(50vw - var(--marker-radius)), calc(${y}px - var(--marker-radius)))`
   marker.style.animationDuration = newAnimationDuration + 's'
@@ -60,10 +47,8 @@ var moveMarker = (event) => {
 
 var modulateAudio = (event) => {
   if (!sourceNode) return
-
   var percentX = (event.x || event.touches[0].clientX) / window.innerWidth
   var percentY = (event.y || event.touches[0].clientY) / window.innerHeight
-
   sourceNode.playbackRate.linearRampToValueAtTime(.5 + percentY, audioContext.currentTime + .1)
 }
 
@@ -73,14 +58,16 @@ var mouseMove = (event) => {
   modulateAudio(event)
 }
 
-window.addEventListener('mousedown', (event) => {
+document.body.addEventListener('mousedown', (event) => {
   document.body.style.cursor = 'crosshair'
   window.onmousemove = mouseMove
 })
 
-window.addEventListener('mouseup', (event) => {
+document.body.addEventListener('mouseup', (event) => {
   document.body.style.cursor = 'unset'
   window.onmousemove = null
 })
 
-window.ontouchstart = mouseMove
+btnStart.onmousedown = btnStart.ontouchstart = (e) => e.stopPropagation()
+document.body.onmousedown = document.body.ontouchstart = mouseMove
+document.body.onclick = () => console.log('click')
